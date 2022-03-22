0fe1765a334221a2793beb27df845e13562d39ad
Kirill Polushkin <polushkin_kirya@mail.ru>
2022-03-22 15:47:13 +0700

    add exitcodes

--- src/libspichki/steps.c
+++ src/libspichki/steps.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <libspichki/const.h>
 #include <libspichki/start.h>
 
 int makeStep(int* i)
 {
     int n;
     printf("Возьмите от 1 до 10 спичек:\n>");
-    char str[10];
-    fgets(str, 10, stdin);
+    char str[128];
+    fgets(str, 128, stdin);
+	if (str[0] == 'q') return EXITCODE_FORCED;
     n = atoi(str);
     if (n >= 1 && n <= 10 && n <= *i) {
         printf("Вы взяли %d\n", n);
         *i -= n;
     } else {
         printf("Пожалуйста, введите правильное число спичек!\n");
-        return EXIT_FAILURE;
+        return EXITCODE_FAILURE;
     }
-    return EXIT_SUCCESS;
+    return EXITCODE_SUCCESS;
 }
 
 void makeStepComputer(int* i)
