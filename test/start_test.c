f024f2fcf6ca90836a40f7d694cd9b249c2e591e
Alexey Bychik <bychik01@mail.ru>
2022-03-28 22:43:59 +0700

    formnat'

--- test/start_test.c
+++ test/start_test.c
@@ -6,9 +6,8 @@
 
 CTEST(suite_start, getRandom)
 {
-	for (int i = 0; i < 10; i++)
-	{
-		int n = getRandom();
-		ASSERT_TRUE(n > 0 && n <= 10);
-	}
+    for (int i = 0; i < 10; i++) {
+        int n = getRandom();
+        ASSERT_TRUE(n > 0 && n <= 10);
+    }
 }
